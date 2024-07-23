29368dcc9dd5122f51e2f9f7bd8ec71fd83eb9c9
Hyunji An <hyunji0322123@gmail.com>
2024-07-23 19:56:26 +0900

    Initial Commit

---
+++ ShakeToRing/ShakeToRingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShakeToRingApp.swift
+//  ShakeToRing
+//
+//  Created by An Hyunji on 7/23/24.
+//
+
+import SwiftUI
+
+@main
+struct ShakeToRingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
